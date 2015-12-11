0d5d4170207c3b97ee7fa1824f59973a6741b8ac
RoaddogLabs <dstevens@roaddog.com>
2015-12-11 01:27:12 -0800

    changed Z homing to for IDE 1.6.6
    Changed the Z max feedrate and max acceleration so it compiles under Arduino 1.6.6

--- Marlin-Marlin_v1_Baja RAMPS/Marlin/Configuration.h
+++ Marlin-Marlin_v1_Baja RAMPS/Marlin/Configuration.h
@@ -452,8 +452,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 // default settings
 
 #define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,4000,455}  // default steps per unit for Baja 
-#define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 25}    // (mm/sec)
-#define DEFAULT_MAX_ACCELERATION      {4000,4000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
+#define DEFAULT_MAX_FEEDRATE          {500, 500, 2, 25}    // (mm/sec)
+#define DEFAULT_MAX_ACCELERATION      {4000,4000,20,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
 #define DEFAULT_ACCELERATION          1000    // X, Y, Z and E max acceleration in mm/s^2 for printing moves default was 3000
 #define DEFAULT_RETRACT_ACCELERATION  3000   // X, Y, Z and E max acceleration in mm/s^2 for retracts
